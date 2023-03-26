81fa7bc79b777b47a3dd052e7d716995ded00387
elifyguler <elifguler93@yandex.com>
2023-03-26 15:18:20 +0100

    first commit

---
+++ movies.js
@@ -0,0 +1,30 @@
+let movieData = {
+    "Inception": {
+      plot: "A skilled thief is recruited to steal secrets from people's subconscious while they dream.",
+      rating: 8.8,
+      year: 2010,
+      cast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+      runtime: 148,
+    },
+    "The Dark Night": {
+      year: 2008,
+      plot: "Batman must face his most dangerous enemy yet: The Joker, who is wreaking havoc on Gotham City.",
+      cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+      runtime: 152,
+      rating: 9.0,
+    },
+    "The Grand Budapest Hotel": {
+      rating: 8.1,
+      runtime: 159,
+      year: 2014,
+      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+    },
+    "Pulp Fiction": {
+      rating: 8.9,
+      runtime: 154,
+      year: 1994,
+      plot: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+      cast: ["John Travolta", "Samuel L. Jackson", "Uma Thurman", "Harvey Keitel", "Tim Roth", "Amanda Plummer", "Maria de Medeiros", "Ving Rhames", "Eric Stoltz", "Rosanna Arquette", "Christopher Walken", "Bruce Willis"],
+    },
+  };
