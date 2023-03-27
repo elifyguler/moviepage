a28d795610f2ecd5fdf5057e583e2364a183a28b
elifyguler <elifguler93@yandex.com>
2023-03-27 10:25:37 +0100

    second commit

--- movies.js
+++ movies.js
@@ -1,30 +1,35 @@
 let movieData = {
-    "Inception": {
-      plot: "A skilled thief is recruited to steal secrets from people's subconscious while they dream.",
-      rating: 8.8,
-      year: 2010,
-      cast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-      runtime: 148,
-    },
-    "The Dark Night": {
-      year: 2008,
-      plot: "Batman must face his most dangerous enemy yet: The Joker, who is wreaking havoc on Gotham City.",
-      cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
-      runtime: 152,
-      rating: 9.0,
-    },
-    "The Grand Budapest Hotel": {
-      rating: 8.1,
-      runtime: 159,
-      year: 2014,
-      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
-      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
-    },
-    "Pulp Fiction": {
-      rating: 8.9,
-      runtime: 154,
-      year: 1994,
-      plot: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-      cast: ["John Travolta", "Samuel L. Jackson", "Uma Thurman", "Harvey Keitel", "Tim Roth", "Amanda Plummer", "Maria de Medeiros", "Ving Rhames", "Eric Stoltz", "Rosanna Arquette", "Christopher Walken", "Bruce Willis"],
-    },
-  };
+  "The Darjeeling Limited": {
+    plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+    cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+    runtime: 151,
+    rating: 7.2,
+    year: 2007,
+  },
+  "The Royal Tenenbaums": {
+    plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+    rating: 7.6,
+    year: 2001,
+    cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+    runtime: 170,
+  },
+  "Fantastic Mr. Fox": {
+    year: 2009,
+    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+    cast: [
+      "George Clooney",
+      "Meryl Streep",
+      "Bill Murray",
+      "Jason Schwartzman",
+    ],
+    runtime: 147,
+    rating: 7.9,
+  },
+  "The Grand Budapest Hotel": {
+    rating: 8.1,
+    runtime: 159,
+    year: 2014,
+    plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+    cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+  },
+};
